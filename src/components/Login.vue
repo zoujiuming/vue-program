0c33923de4841af5a9c636a61f880d8c49035799
zoujiuming <zjmwx0315@163.com>
2019-07-10 16:26:36 +0800

    完成了login的样式: git commit -m 完成了login的样式
    
    
    git commit -m 完成了login的样式:

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,19 @@
 <template>
-<div class="login">
-  <el-form ref="form" :model="form" label-width="80px">
-    <el-form-item label="用户名">
-      <el-input v-model="form.name"></el-input>
-    </el-form-item>
+  <div class="login">
+    <el-form ref="form" :model="form" label-width="80px">
+      <img src="../assets/avatar.jpg" alt="">
+  <el-form-item label="用户名">
+    <el-input v-model="form.username"></el-input>
+  </el-form-item>
     <el-form-item label="密码">
-      <el-input v-model="form.name"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary">登录</el-button>
-      <el-button>重置</el-button>
-    </el-form-item>
-  </el-form>
-    </div>
+    <el-input v-model="form.password"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary">登录</el-button>
+    <el-button>重置</el-button>
+  </el-form-item>
+</el-form>
+  </div>
 </template>
 
 <script>
@@ -20,7 +21,8 @@ export default {
   data () {
     return {
       form: {
-        name: ''
+        username: '',
+        password: ''
       }
     }
   }
@@ -29,28 +31,29 @@ export default {
 
 <style lang="less">
 .login {
-  width: 100%;
+  widows: 100%;
   height: 100%;
   background-color: #2d434c;
   overflow: hidden;
   .el-form {
     width: 400px;
-    padding: 75px 40px 15px 40px;
     background-color: #fff;
     margin: 200px auto;
+    padding: 75px 40px 15px;
     border-radius: 20px;
     position: relative;
     img {
       position: absolute;
-      top: -80px;
       left: 50%;
       transform: translateX(-50%);
+      top:-75px;
       border-radius: 50%;
-      border: 10px solid #fff;
+      border: 8px solid #fff;
     }
+  .el-button:last-child{
+    margin-left: 100px;
   }
-  .reset {
-    margin-left: 80px;
   }
+
 }
 </style>
