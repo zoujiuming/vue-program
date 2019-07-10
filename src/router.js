5b3ca4ef470a0eb3d2fdfbfb36a75dae976c7bec
zoujiuming <zjmwx0315@163.com>
2019-07-10 21:02:41 +0800

    增加了登录拦截

--- src/router.js
+++ src/router.js
@@ -10,5 +10,19 @@ const router = new VueRouter({
     { path: '/login', component: Login, name: 'login' }
   ]
 })
+router.beforeEach((to, from, next) => {
+  //  to只要不是去login,都应该判断是否有token
+  //  to如果要是去/login,不管有没有token 可以直接next()
+  //  to不是去,判断有没有token直接放行,否则去login
+  const token = localStorage.getItem('token')
+  if (to.path === '/login') {
+    return next() // 去login 就去
+  }
+  if (token) { // 有令牌就随意走
+    next()
+  } else {
+    next('/login') // 啥都没有就去登录去
+  }
+})
 
 export default router
