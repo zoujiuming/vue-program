8ebda99537f625f4dee41dab97371e7d32f5f0de
zoujiuming <zjmwx0315@163.com>
2019-07-10 17:36:52 +0800

    表单校验完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="login">
-    <el-form ref="form" :model="form" label-width="80px">
+    <el-form :rules="rules" ref="form" :model="form" label-width="80px" status-icon>
       <img src="../assets/avatar.jpg" alt="">
-  <el-form-item label="用户名">
-    <el-input v-model="form.username"></el-input>
+  <el-form-item label="用户名" prop="username">
+    <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
   </el-form-item>
-    <el-form-item label="密码">
-    <el-input v-model="form.password"></el-input>
+    <el-form-item label="密码"  prop="password">
+    <el-input v-model="form.password" type="password" placeholder="请输入密码"></el-input>
   </el-form-item>
   <el-form-item>
-    <el-button type="primary">登录</el-button>
-    <el-button>重置</el-button>
+    <el-button type="primary" @click="submitForm">登录</el-button>
+    <el-button @click="resetForm">重置</el-button>
   </el-form-item>
 </el-form>
   </div>
@@ -23,7 +23,33 @@ export default {
       form: {
         username: '',
         password: ''
+      },
+      //  表单校验
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'change' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'change' }
+        ],
+        password: [
+          { required: true, message: '请输入用户名', trigger: 'change' },
+          { min: 10, max: 14, message: '长度在 10 到 14 个字符', trigger: 'change' }
+        ]
       }
+    }
+  },
+  methods: {
+    resetForm () {
+      // 获取表单组件,调用方法
+      this.$refs.form.resetFields()
+    },
+    submitForm () {
+      this.$refs.form.validate(function (isvalid, obj) {
+        if (isvalid) {
+          console.log('发送ajax')
+        } else {
+          return false
+        }
+      })
     }
   }
 }
